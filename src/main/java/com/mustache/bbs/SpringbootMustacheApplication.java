dc0d7264e39e4b65a46c1c90b77fa953ad9395ca
dohyu <dohyunre702@gmail.com>
2022-12-25 18:27:45 +0900

    first commit

---
+++ src/main/java/com/mustache/bbs/SpringbootMustacheApplication.java
@@ -0,0 +1,13 @@
+package com.mustache.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMustacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMustacheApplication.class, args);
+    }
+
+}
