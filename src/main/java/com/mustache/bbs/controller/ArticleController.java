95984cc51a269b620b9b20a9e865d841a17eeb0a
dohyu <dohyunre702@gmail.com>
2022-12-25 20:05:58 +0900

    [add] 수정 요청받기, 데이터 수정하기, 데이터 삭제하기 - [GET] edit() - [PATCH] update() *form에서 put 지원 x - [GET] delete() *html에서 delete 지원 x

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -19,7 +20,7 @@ public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
 
-    //1. 새 글 불러오기 : url로 들어오면 articles/new.mustache 출력
+    //1. 새 글 작성 페이지 불러오기 : url로 들어오면 articles/new.mustache 출력
     @GetMapping(value = "/articles/new")
     public String newArticleForm() {
         return "articles/new";
@@ -38,7 +39,7 @@ public class ArticleController {
         return "redirect:/articles/" + saved.getId();
     }
 
-    //3. URL 조회하기
+    //3. URL 조회하기 : 엔티티를 article 변수로 만들어서 show 페이지에 출력
     @GetMapping("/articles/{id}")
     public String read(@PathVariable Long id, Model model) {
         //1) id로 데이터 가져오기
@@ -52,7 +53,7 @@ public class ArticleController {
 
     }
 
-    //4. 전체 조회하기
+    //4. 전체 조회하기 : 엔티티를 articleList로 만들어서 index 페이지에 출력
     @GetMapping("/articles")
     public String getAll(Model model) {//articles로 요청을 받으면 getAll 메서드 작동
 
@@ -65,5 +66,48 @@ public class ArticleController {
         //3) 뷰 페이지 설정
         return "articles/index";
     }
+
+    //5. 수정 요청 받기 : 엔티티를 article 변수로 만들어서 edit 페이지에 출력
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        model.addAttribute("article", articleEntity); //수정할 articleEntity의 변수명 = article
+        return "articles/edit";
+    }
+
+    //6. 데이터 수정하기 : edit.mustach에서 articles/update 요청 들어 오면 로직 진행
+    // ** put을 폼이 받지 못해 patch 사용
+    @PatchMapping("/articles/update")
+    public String update(ArticleDto dto) {
+        //1) DTO 엔티티로 변환
+        Article articleEntity = dto.toEntity();
+
+        //2) ID가 DB에 있는지 찾기 아니면 null 반환
+        Article targetEntity = articleRepository.findById(articleEntity.getId()).orElse(null);
+
+        //3) DB에 기존 데이터가 있다면 값 변경
+        if (targetEntity != null) {
+            articleRepository.save(articleEntity);
+        }
+
+        //4) 수정 결과 페이지로 리다이렉트
+        return "redirect:/articles";
+    }
+
+    //7. 데이터 삭제하기 : html에서 delete요청 지원 x > deleteMapping 사용불가
+    @GetMapping(value = "/articles/{id}/delete")
+    public String delete(@PathVariable Long id) {
+        //1) 삭제대상 찾기
+        Article deleteEntity = articleRepository.findById(id).orElse(null);
+
+        //2) DB에 대상이 있다면 제거
+        if (deleteEntity != null) {
+            articleRepository.delete(deleteEntity);
+        }
+
+        //3) 결과 리다이렉트
+        return "redirect:/articles";
+    }
+
 
 }
