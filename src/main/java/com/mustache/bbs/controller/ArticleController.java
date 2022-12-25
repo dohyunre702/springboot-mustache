795a60af8fd4f57c0a030108ba5edac3703de239
dohyu <dohyunre702@gmail.com>
2022-12-25 19:41:40 +0900

    [add] URL 조회하기, 전체 조회하기 - [GET] read() - [GET] getAll()

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -5,8 +5,12 @@ import com.mustache.bbs.domain.dto.ArticleDto;
 import com.mustache.bbs.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class ArticleController {
@@ -33,4 +37,33 @@ public class ArticleController {
         //3) 리다이렉트. 메서드 작동 종료 후 페이지 반환
         return "redirect:/articles/" + saved.getId();
     }
+
+    //3. URL 조회하기
+    @GetMapping("/articles/{id}")
+    public String read(@PathVariable Long id, Model model) {
+        //1) id로 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        //2) 가져온 데이터를 모델에 등록
+        model.addAttribute("article", articleEntity);
+
+        //3) 보여줄 페이지 선택
+        return "articles/show";
+
+    }
+
+    //4. 전체 조회하기
+    @GetMapping("/articles")
+    public String getAll(Model model) {//articles로 요청을 받으면 getAll 메서드 작동
+
+        //1) 모든 articles를 가져온다
+        List<Article> articleEntityList = (List<Article>) articleRepository.findAll();
+
+        //2) 가져온 articles 리스트를 뷰로 전달 (모델을 뷰로 전달)
+        model.addAttribute("articleList", articleEntityList);
+
+        //3) 뷰 페이지 설정
+        return "articles/index";
+    }
+
 }
--- src/main/java/com/mustache/bbs/repository/ArticleRepository.java
+++ src/main/java/com/mustache/bbs/repository/ArticleRepository.java
@@ -2,8 +2,6 @@ package com.mustache.bbs.repository;
 
 import com.mustache.bbs.domain.Article;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 public interface ArticleRepository extends CrudRepository<Article, Long> {
