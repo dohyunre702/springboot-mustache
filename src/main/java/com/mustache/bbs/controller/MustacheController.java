c0895e23526f0c0a3081c4f57f3c0f904a9c83ae
dohyu <dohyunre702@gmail.com>
2022-12-25 18:29:14 +0900

    initial commit

--- src/main/java/com/mustache/bbs/controller/MustacheController.java
+++ src/main/java/com/mustache/bbs/controller/MustacheController.java
@@ -1,2 +1,16 @@
-package com.mustache.bbs.controller;public class MustacheController {
+package com.mustache.bbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MustacheController {
+
+    //test용
+    @GetMapping(value = "/hello")
+    public String hello(Model model) {
+        model.addAttribute("username", "dhlee");
+        return "greetings";
+    }
 }
